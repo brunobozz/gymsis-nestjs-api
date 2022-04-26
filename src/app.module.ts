a403029a167df55a9326774ac6e1ec366c8e1c45
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-04-25 23:00:03 -0300

    created config to conect on remote POSTGRES

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-3-218-171-44.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'spyhijwmmuwcvv',
+      password:
+        '12f16037088072a21915ed2274380f7f162e7a4f307da89150fdea9349691648',
+      database: 'd2q3u72j43bkdi',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
